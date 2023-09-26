f8718306b65dea27cf1663a874442f0282d32e6f
srwesteban <sr.w@hotmail.com>
2023-09-26 00:47:39 -0500

    petmap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,9 @@
-import {useAuth} from '../context/AuthContext';
 import NavbarHome from './NavbarHome';
 import Mapa from './Mapas/Mapa';
 import Footer from './Footer';
 
 
 export function Home() {
-  const { logout, user } = useAuth();
-
-  console.log(user);
-  const handleLogout = async () => {
-    try {
-      await logout();
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
 
   return (
     <div>
