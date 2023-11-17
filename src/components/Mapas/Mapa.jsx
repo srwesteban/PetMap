c1fb5ac89d6a45c1fea661faa9f7a7233156ed32
srwesteban <sr.w@hotmail.com>
2023-11-16 20:42:11 -0500

    a

--- src/components/Mapas/Mapa.jsx
+++ src/components/Mapas/Mapa.jsx
@@ -16,48 +16,36 @@ let iconUbicacion = new L.icon({
 
 export default function Mapa() {
   const [userLocation, setUserLocation] = useState(null);
-  const punto2Location = [1.22, -77.29]; // Cambia estas coordenadas según tu necesidad
+  const [punto2Location, setPunto2Location] = useState({ lat: 1.22, lon: -77.29 });
+  const [inputCoords, setInputCoords] = useState('');
 
-  // Función para obtener la ubicación del usuario
-  const getUserLocation = () => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        const { latitude, longitude } = position.coords;
-        setUserLocation([latitude, longitude]);
-        // Enviar las coordenadas al servidor mediante una solicitud POST
-        sendCoordinatesToServer(latitude, longitude);
-      });
+  const handleCoordsInputChange = (e) => {
+    setInputCoords(e.target.value);
+  };
+
+  const handlePunto2Submit = (e) => {
+    e.preventDefault();
+
+    // Dividir las coordenadas por coma y convertirlas a números
+    const [lat, lon] = inputCoords.split(',').map(coord => parseFloat(coord.trim()));
+
+    // Verificar si las coordenadas son válidas
+    if (!isNaN(lat) && !isNaN(lon)) {
+      setPunto2Location({ lat, lon });
+      // Puedes realizar acciones adicionales aquí si es necesario
+    } else {
+      console.error('Coordenadas no válidas. Por favor, ingrese números válidos.');
     }
   };
 
-  // Función para enviar las coordenadas al servidor
-  const sendCoordinatesToServer = (latitude, longitude) => {
-    axios.post('http://localhost:3000/api/coordenadas', {
-      id: 1,
-      name: 'Punto 1',
-      lat: latitude,
-      lon: longitude
-    })
-      .then(response => {
-        console.log('Coordenadas enviadas al servidor con éxito');
-      })
-      .catch(error => {
-        console.error('Error al enviar coordenadas al servidor', error);
-      });
-  };
-
-  useEffect(() => {
-    getUserLocation();
-  }, []);
-
   return (
-    <div className="w-full h-screen mb-10 max-h-[76vh] z-0">
+    <div className="relative w-full h-screen mb-10 max-h-[76vh] z-0">
       <MapContainer
         center={userLocation || [1.2136, -77.2811]}
         zoom={14}
         scrollWheelZoom={true}
         id='mapa'
-        style={{ maxWidth: '100%', height: '100%' }}
+        style={{ width: '100%', height: '60vh' }}
       >
         <TileLayer attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
@@ -69,12 +57,32 @@ export default function Mapa() {
           </Marker>
         )}
 
-        <Marker position={punto2Location} icon={iconUbicacion}>
-          <Popup>
-            Punto 2
-          </Popup>
-        </Marker>
+        {(!isNaN(punto2Location.lat) && !isNaN(punto2Location.lon)) && (
+          <Marker position={[punto2Location.lat, punto2Location.lon]} icon={iconUbicacion}>
+            <Popup>
+              Punto 2
+            </Popup>
+          </Marker>
+        )}
       </MapContainer>
+
+      <div className="absolute bottom-0 left-0 w-full bg-black p-4 text-white">
+        <form onSubmit={handlePunto2Submit}>
+          <label className="block mb-2 text-lg">
+            Ingrese las coordenadas (latitud, longitud):
+            <input
+              type="text"
+              name="coords"
+              placeholder="Ejemplo: 1.2136,-77.2811"
+              value={inputCoords}
+              onChange={handleCoordsInputChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </label>
+          <button type="submit" className="bg-blue-500 text-white p-2 rounded">Actualizar Punto 2</button>
+        </form>
+      </div>
     </div>
   );
 }
